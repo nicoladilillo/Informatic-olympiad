57f0f7b5916ebf1a1917337ddcbb1788cb189f97
Dilillo Nicola <nicoladilillo98@gmail.com>
2016-04-07 20:36:19 +0200

    Finish Nimbus

--- Nimbus/Nimbus.cpp
+++ Nimbus/Nimbus.cpp
@@ -2,27 +2,41 @@
 # include <iostream>
 # include <algorithm>
 
+using namespace std;
+
 struct intervallo {
 	int inizio, fine;
 };
 
-bool controllo( const intervallo &a, const intervallo int &b) {
-	return a.fine < b.fine;
+bool controllo(intervallo a, intervallo b) {
+		return a.fine < b.fine;
 }
 
 int n;
 intervallo giri[1000];
 
-int main()
+main()
 {
 	ifstream in("input.txt");
-	ofstream on("output.txt");
-	in >> N;
-	for( i=0; i<n; i++) 
+	ofstream out("output.txt");
+	in >> n;
+	for(int i=0; i<n; i++) 
 	    in >> giri[i].inizio >> giri[i].fine;
 	    
-	sort(giri, giri+n, controllo)
+	sort(giri, giri+n, controllo);
 	
-	int fine_attuale
+	int fine_attuale = giri[0].inizio;
+	int caramelle = 1;
+	for (int i=1; i<n; i++)
+	{
+		if (fine_attuale < giri[i].inizio)
+		{
+			fine_attuale = giri[i].fine;
+			caramelle++;
+		}
+	}
+
+	out << caramelle << endl;
+	return 0;
 
 }
